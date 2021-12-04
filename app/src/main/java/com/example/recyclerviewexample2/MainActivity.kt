e8818896d34634793094855a3c7a58db9e32ddfe
byTurker <mr_turker@hotmail.com>
2021-12-04 13:39:12 +0300

    Init DataBinding

--- app/src/main/java/com/example/recyclerviewexample2/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexample2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.recyclerviewexample2
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +15,15 @@ class MainActivity : AppCompatActivity() {
 
         my_recycler_view.setBackgroundColor(Color.YELLOW)
         my_recycler_view.layoutManager = LinearLayoutManager(this)
-        my_recycler_view.adapter = MyRecyclerViewAdapter(fruitsList)
+        my_recycler_view.adapter = MyRecyclerViewAdapter(fruitsList) { selectedFruitItem: Fruit ->
+            listItemClicked(
+                selectedFruitItem
+            )
+        }
+    }
+
+    private fun listItemClicked(fruit: Fruit){
+        Toast.makeText(this@MainActivity,
+            "Supplier name is ${fruit.supplier}", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/recyclerviewexample2/MyRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recyclerviewexample2/MyRecyclerViewAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item.view.*
 
-class MyRecyclerViewAdapter(val fruitsList:List<Fruit>): RecyclerView.Adapter<MyViewHolder>() {
+class MyRecyclerViewAdapter(private val fruitsList:List<Fruit>, private val clickListener:(Fruit)->Unit): RecyclerView.Adapter<MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -15,8 +15,7 @@ class MyRecyclerViewAdapter(val fruitsList:List<Fruit>): RecyclerView.Adapter<My
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val fruit = fruitsList[position]
-        holder.view.name_text_view.text = fruit.name
+        holder.bind(fruitsList[position],clickListener)
     }
 
     override fun getItemCount(): Int {
@@ -24,6 +23,11 @@ class MyRecyclerViewAdapter(val fruitsList:List<Fruit>): RecyclerView.Adapter<My
     }
 }
 
-class MyViewHolder(val view : View) : RecyclerView.ViewHolder(view){
-
+class MyViewHolder(private val view : View) : RecyclerView.ViewHolder(view){
+    fun bind(fruit: Fruit, clickListener:(Fruit)->Unit){
+        view.name_text_view.text = fruit.name
+        view.setOnClickListener {
+            clickListener(fruit)
+        }
+    }
 }
